02bb54ebce2bfd53e3628180ae4176c5e759dbcd
Vikuuu <adivik672@gmail.com>
2025-12-13 18:58:40 +0530

    feat: get the name of files to stdout

--- asm-ls/asmls.asm
+++ asm-ls/asmls.asm
@@ -15,6 +15,9 @@
 SECTION .data
     error_msg       db "Error encountered", 0Ah
     ERROR_MSG_LEN   equ $-error_msg
+    new_line        db 0Ah
+    NEW_LINE_LEN    equ $-new_line
+    dot             db ".", 0
 
 ;--------------------------------------------------
 ;  UNINITIALISED DATA
@@ -22,6 +25,11 @@ SECTION .data
 SECTION .bss
     current_path_buf     resb 256
     CURRENT_PATH_BUF_LEN equ $-current_path_buf
+
+    dir_buff     resb 4096
+    DIR_BUFF_LEN equ $-dir_buff
+
+    dir_fd resq 1
 
 ;--------------------------------------------------
 ;  PROCEDURES
@@ -96,7 +104,7 @@ Getwd:
 
 ;--------------------------------------------------
 ; Print         : print the given message
-; In            : RSI(buff offset), rdx(buff len)
+; In            : RSI(buff offset), RDX(buff len)
 ; Returns       : R15(return code)
 ; Modifies      : Nothing
 ; Calls         : Syscall sys_write
@@ -105,14 +113,47 @@ Getwd:
 Print:
     push rax ; Save caller's register
     push rbx
+    push rsi
+    push rdx
 
     mov rax, 1 ; Syscall sys_write code
-    mov rax, 1 ; File descriptor 1: Standard out
+    mov rdi, 1 ; File descriptor 1: Standard out
     syscall
 
     mov r15, rax
 
+    pop rdx
+    pop rsi
     pop rbx ; Restore caller's register
+    pop rax
+
+    ret
+
+;--------------------------------------------------
+; PrintNewLine  : prints new line
+; In            : Nothing
+; Returns       : R15(return code)
+; Modifies      : Nothing
+; Calls         : Syscall sys_write
+; Description   : Prints the New line
+
+PrintNewLine:
+    push rax ; Save caller's register
+    push rdi
+    push rsi
+    push rdx
+
+    mov rax, 1 ; Syscall sys_write code
+    mov rdi, 1 ; File descriptor 1: Standard out
+    mov rsi, new_line ; Offset of new_line buf
+    mov rdx, NEW_LINE_LEN ; Length of the buf
+    syscall
+
+    mov r15, rax ; Save the return code
+
+    pop rdx ; Restore caller's register
+    pop rsi
+    pop rdi
     pop rax
 
     ret
@@ -121,14 +162,58 @@ Print:
 GLOBAL _start ; Linked needs this to find starting point
 
 _start:
+    mov rax, 257 ; Syscall sys_openat code
+    mov rdi, -100 ; value of AT_FDCWD
+    mov rsi, dot ; "."
+    mov rdx, 0x10000 ; O_DIRECTORY
+    syscall
 
-    call Getwd
-    cmp r15, 0   ; Compare the return code
-    jl ErrorExit ; If -ve return value, exit with error msg
-    mov rsi, current_path_buf ; Pointer to buffer
-    mov rdx, r15              ; Length of buffer
-    call Print
-    cmp r15, 0   ; Compare the return code
-    jl ErrorExit ; If -ve return code, exit with error msg
+    cmp rax, -1 ; Compare return code
+    je ErrorExit
 
-    call Exit
+    mov [dir_fd], rax ; Save the returned fd
+
+ReadDir:
+    mov rax, 217 ; Syscall sys_getdents64 code
+    mov rdi, [dir_fd]; file descriptor
+    lea rsi, [dir_buff] ; Load effective address of `dir_buff`
+    mov rdx, DIR_BUFF_LEN ; count
+    syscall
+
+    cmp rax, 0 ; compare the return code
+    jl ErrorExit ; if -ve, error encountered
+    je done ; if 0 we are done
+
+    mov rcx, rax ; move bytes_read
+    lea rbx, [dir_buff] ; load efeective address of `dir_buff`
+
+    next:
+        movzx rdx, word [rbx + 16] ; move to the d_reclen
+        cmp rdx, 0 ; compare the d_reclen value
+        jz done ; if zero we are done
+        lea rsi, [rbx + 19] ; move to the d_name
+
+        push rdx ; save the rdx
+        xor r8, r8; clear out r8
+        .find:
+            cmp byte [rsi + r8], 0 ; compare with the null_byte
+            je .len_found ; we found the null_byte jump to len_found
+            inc r8 ; increment the length
+            jmp .find ; reiterate to find the null_byte
+        .len_found:
+            ; save the length of the file name in rdx
+            ; preparing for the sys_write call
+            ; file name already pointed by rsi
+            mov rdx, r8 
+            call Print
+            call PrintNewLine
+        pop rdx ; restore the rdx value
+        
+        add rbx, rdx ; move the file name ahead by the d_reclen
+        jmp next
+    done:
+        mov rax, 3 ; sys_close
+        mov rdi, [dir_fd]
+        syscall 
+
+        call Exit
